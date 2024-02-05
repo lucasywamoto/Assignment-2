aad756a081f9cce1e02ede63945367478cfd00ba
Lucas Ywamoto <92839243+lucasywamoto@users.noreply.github.com>
2024-02-05 14:10:22 -0500

    Changes

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 const studentNames = ["Alice", "Bob", "Charlie", "David", "Eve", "Frank", "Grace", "Hannah", "Ian", "Jasmine"];
 let i = 0;
 
-sendMessages = (array) => {
+sendMessages = (array, message) => {
     array.forEach((name, index) => {
+        var jsonDate = (new Date()).toJSON();
         setTimeout(() => {
-            document.write(name + ": Hi, " + name + "! Welcome to JavaScript class 🙂<br>");
+            document.write("<small>" + jsonDate + "</small><br>");
+            document.write(jsonDate + name + ": Hi, " + name + "! " + message + "🙂<br><br>");
         }, 500 * index);
     });
 }
 
-sendMessages(studentNames);
+sendMessages(studentNames, "Welcome to JavaScript class");
